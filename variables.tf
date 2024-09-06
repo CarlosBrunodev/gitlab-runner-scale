e1a050933868137117939d09bf577a3ab57f2f86
carlos.bruno.ext <carlos.bruno.ext@e-ai.com.br>
2024-09-06 16:57:45 -0400

    remove sensitive data

--- variables.tf
+++ variables.tf
@@ -14,14 +14,14 @@ variable "gitlab_runner_launch-template" {
     description = "Mapa de objetos de variaveis para definicao de um Launch template"
     type = map(object({
       name                = string
-      ami_id              = optional(string,"ami-052efd3df9dad4825")
-      instance_type       = optional(string,"t3.xlarge")
+      ami_id              = optional(string,"")
+      instance_type       = optional(string,"")
       key_name            = string
       subnet_id           = string
       security_group_id   = string
       gitlab_runner_url   = string
       gitlab_runner_token = string
-      iam_istance_profile = optional(string,"arn:aws:iam::681951013428:role/gitlab-runner")
+      iam_istance_profile = optional(string,"")
     }))
   
 }
